031e6427e8b8e18b07fa6a7b12b5e9092237c84a
Alfred Peter <alpeter91@gmail.com>
2024-02-17 00:02:04 +0300

    Add user input

---
+++ 02_user_input.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+  char name;
+  printf("Hello there!\n");
+  printf("What is your name?\n");
+  scanf("%s", name);
+  printf("Hi %s", name);
+
+  return 0;
+}
+
